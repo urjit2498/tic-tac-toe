443c0af33e8e2c3e571d978c8d75b47fefd88dea
Parth Kantaria <parth.kantaria@sublimedatasys.com>
2020-01-10 12:59:00 +0530

    Pushed local tictactoe repo

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="google.com">
+          <b>Tic-Tac-Toe</b>
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
